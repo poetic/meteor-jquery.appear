8903011c1e4b78db7e50500e474cac6bba878cd7
Dodie324 <drmoore324@gmail.com>
2015-09-03 15:58:22 -0500

    0.0.4

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'poetic:jquery.appear',
-  version: '0.0.3',
+  version: '0.0.4',
   // Brief, one-line summary of the package.
   summary: 'Meteor wrapper for jQuery.appear',
   // URL to the Git repository containing the source code for this package.
@@ -11,7 +11,7 @@ Package.describe({
 });
 
 Npm.depends({
-  'jquery-appear-poetic': '0.3.6'
+  'jquery-appear-poetic': '0.3.7'
 });
 
 Package.onUse(function(api) {
